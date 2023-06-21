ff37e4f20ff8ddcdba0bb8dbdbe51ceb1f4dbe2c
Oly Su <taiya1229@gmail.com>
2023-06-21 18:59:25 +1000

    ✨ Icons

--- src/app/_components/HomePage/HomePage.tsx
+++ src/app/_components/HomePage/HomePage.tsx
@@ -1,10 +1,17 @@
 import type { FC } from 'react'
 import Robot from '@/components/Robot'
+import Icon from '@/components/Icon'
 
 const HomePage: FC = () => (
   <div>
     <p>Hello, world!</p>
     <Robot />
+    <Icon.Bad />
+    <Icon.Check />
+    <Icon.Expand />
+    <Icon.Good />
+    <Icon.Missing />
+    <Icon.Sparkles />
   </div>
 )
 
---
+++ src/components/Icon/index.ts
@@ -0,0 +1,17 @@
+import Bad from './Bad'
+import Check from './Check'
+import Expand from './Expand'
+import Good from './Good'
+import Missing from './Missing'
+import Sparkles from './Sparkles'
+
+const Icon = {
+  Bad,
+  Check,
+  Expand,
+  Good,
+  Missing,
+  Sparkles,
+}
+
+export default Icon
